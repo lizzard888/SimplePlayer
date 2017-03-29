197891bba30d43ab3d51fd2b563d73ea51de40a3
lizzard888 <lizzard@autograf.pl>
2017-03-30 01:48:13 +0200

    All controls seems to be ok. Added volume control.

--- Source/main.c
+++ Source/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <gst/gst.h>
 #include <glib.h>
+
+struct Data{
+  GstBus *bus;
+  GstElement *pipeline, *volume;
+};
 
 static gboolean
 bus_call (GstBus     *bus,
@@ -9,8 +15,6 @@ bus_call (GstBus     *bus,
           gpointer    data)
 {
   GMainLoop *loop = (GMainLoop *) data;
-
-  g_print("Message received!\n");
 
   switch (GST_MESSAGE_TYPE (msg)) {
 
@@ -39,38 +43,49 @@ bus_call (GstBus     *bus,
 }
 
 void *
-player_controls_function(GstElement *pipeline)
+player_controls_function(struct Data *data_instance)
 {
-    GstBus *bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
     char str;
     while(TRUE){
       scanf("%c", &str);
       switch (str) {
         case 'P':
         {
-          if(GST_STATE_TARGET(pipeline) == GST_STATE_PLAYING){
-            gst_element_set_state (pipeline, GST_STATE_PAUSED);
+          if(GST_STATE_TARGET(data_instance->pipeline) == GST_STATE_PLAYING){
+            gst_element_set_state (data_instance->pipeline, GST_STATE_PAUSED);
             g_print ("Paused\n");
           }
           else{
-            gst_element_set_state (pipeline, GST_STATE_PLAYING);
+            gst_element_set_state (data_instance->pipeline, GST_STATE_PLAYING);
             g_print ("Running\n");
           }
           break;
         }
         case 'S':
         {
-          gst_bus_post (bus, gst_message_new_eos(NULL));
+          gst_bus_post (data_instance->bus, gst_message_new_eos(NULL));
           break;
         }
         case '+':
         {
-          //TODO
+          gdouble val;
+          g_object_get(G_OBJECT(data_instance->volume), "volume", &val, NULL);
+          val += 0.1;
+          if(val > 3.9)
+            val = 4.0;
+          g_object_set(G_OBJECT (data_instance->volume), "volume", val, NULL);
+          g_print ("Volume: %d%%\n", (int)(((val/1.0)*100)+0.5));
           break;
         }
         case '-':
         {
-          //TODO
+          gdouble val;
+          g_object_get(G_OBJECT(data_instance->volume), "volume", &val, NULL);
+          val -= 0.1;
+          if(val < 0.1)
+            val = 0.0;
+          g_object_set(G_OBJECT (data_instance->volume), "volume", val, NULL);
+          g_print ("Volume: %d%%\n", (int)(((val/1.0)*100)+0.5));
           break;
         }
       }
@@ -86,7 +101,7 @@ main (int   argc,
   GThread *ControlsThread;
   GError *err = NULL;
 
-  GstElement *pipeline, *source, *wavparser, *conv, *sink;
+  GstElement *pipeline, *source, *wavparser, *volume, *conv, *sink;
   GstBus *bus;
   guint bus_watch_id;
 
@@ -110,10 +125,11 @@ main (int   argc,
   pipeline = gst_pipeline_new ("audio-player");
   source   = gst_element_factory_make ("filesrc",       "file-source");
   wavparser  = gst_element_factory_make ("wavparse",      "wav-parser");
+  volume = gst_element_factory_make ("volume",      "volume");
   conv     = gst_element_factory_make ("audioconvert",  "converter");
   sink     = gst_element_factory_make ("autoaudiosink", "audio-output");
 
-  if (!pipeline || !source || !wavparser || !conv || !sink) {
+  if (!pipeline || !source || !wavparser || !volume || !conv || !sink) {
     g_printerr ("One element could not be created. Exiting.\n");
     return -1;
   }
@@ -131,12 +147,11 @@ main (int   argc,
   /* we add all elements into the pipeline */
   /* file-source | wav-parser | converter | alsa-output */
   gst_bin_add_many (GST_BIN (pipeline),
-                    source, wavparser, conv, sink, NULL);
+                    source, wavparser, volume, conv, sink, NULL);
 
   /* we link the elements together */
   /* file-source -> wav-parser -> converter -> alsa-output */
-  //gst_element_link (source, wavparser);
-  gst_element_link_many (source, wavparser, conv, sink, NULL);
+  gst_element_link_many (source, wavparser, volume, conv, sink, NULL);
 
   /* Set the pipeline to appropriate state*/
   g_print ("Now playing: %s\n", argv[1]);
@@ -148,11 +163,19 @@ main (int   argc,
     gst_element_set_state (pipeline, GST_STATE_PAUSED);
     g_print ("Paused\n");
   }
-  if(!(ControlsThread = g_thread_try_new(NULL, (GThreadFunc)player_controls_function, pipeline, &err))){
+
+  struct Data *data_instance = malloc(sizeof(struct Data));
+  data_instance->bus = bus;
+  data_instance->volume = volume;
+  data_instance->pipeline = pipeline;
+
+  if(!(ControlsThread = g_thread_try_new(NULL, (GThreadFunc)player_controls_function, data_instance, &err))){
     g_printerr("Thread create failed: %s\n", err->message );
     g_error_free(err);
     return -1;
   }
+
+  g_object_set(G_OBJECT (volume), "volume", 0.5, NULL);
   g_main_loop_run(loop);
 
   /* Out of the main loop, clean up nicely */
@@ -163,6 +186,8 @@ main (int   argc,
   g_source_remove (bus_watch_id);
   g_main_loop_unref (loop);
   g_thread_unref(ControlsThread);
+
+  free(data_instance);
 
   return 0;
 }
